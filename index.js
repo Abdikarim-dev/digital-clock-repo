4c20f100fb227961159eed9603fed9cb80bbc505
abdikarim-dev <cabdikariim405@gmail.com>
2024-02-05 12:43:50 +0300

    updated in 12:43PM

--- index.js
+++ index.js
@@ -2,14 +2,25 @@
 const hourElement = document.querySelector(".hour");
 const minuteElement = document.querySelector(".minute");
 const secondElement = document.querySelector(".second");
-const monthElement = document.querySelector(".monthName");
-const dayElement = document.querySelector(".dayName");
+const monthElement = document.querySelector(".month");
+const dayElement = document.querySelector(".day");
+const yearElement = document.querySelector(".year");
 
 // Arrays containing names for days of the week and months for easy reference
 const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 const monthNames = [
-  "Jan", "Feb", "Mar", "Apr", "May", "Jun",
-  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
 ];
 
 // Function to update the clock display on the webpage
@@ -20,13 +31,17 @@ const updateClockDisplay = () => {
   // Retrieve the current day and month using the Date object
   let day = dayNames[now.getDay()];
   let month = monthNames[now.getMonth()];
+  let year = now.getFullYear();
 
   // Format hours, minutes, and seconds to ensure they are always displayed as two digits
   let hour = now.getHours() < 10 ? `0${now.getHours()}` : now.getHours();
-  let minute = now.getMinutes() < 10 ? `0${now.getMinutes()}` : now.getMinutes();
-  let second = now.getSeconds() < 10 ? `0${now.getSeconds()}` : `${now.getSeconds()}`;
+  let minute =
+    now.getMinutes() < 10 ? `0${now.getMinutes()}` : now.getMinutes();
+  let second =
+    now.getSeconds() < 10 ? `0${now.getSeconds()}` : `${now.getSeconds()}`;
 
   // Update the HTML content of the selected elements with the current time and date
+  yearElement.innerHTML = year;
   monthElement.innerHTML = month;
   dayElement.innerHTML = day;
   hourElement.innerHTML = hour;
@@ -37,3 +52,7 @@ const updateClockDisplay = () => {
 // Set an interval to call the updateClockDisplay function every 1000 milliseconds (1 second)
 // This ensures the clock on the webpage is updated in real time
 setInterval(updateClockDisplay, 1000);
+
+const date2 = new Date();
+
+console.log(date2.getFullYear());
